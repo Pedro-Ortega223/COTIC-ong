9151f2843cce15457a00285b5aa11b5fe7570a3b
Pedro Ortega Garcia <pedrohenriqueogarcia89@gmail.com>
2025-11-12 13:48:39 -0300

    ultimo

--- js/script.js
+++ js/script.js
@@ -14,3 +14,16 @@ function nextImage(){
     document.getElementById("radio" + count).checked = true;
 
 }
+
+function mostrarSenha(){
+    var inputPass = document.getElementById('senha');
+    var btnShowPass = document.getElementById('btn-senha');
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type','text');
+        btnShowPass.classList.replace('bi-eye-fill','bi-eye-slash-fill');
+    }else{
+        inputPass.setAttribute('type','password');
+        btnShowPass.classList.replace('bi-eye-slash-fill','bi-eye-fill',);
+    }
+}
